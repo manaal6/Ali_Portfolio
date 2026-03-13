0e22fac463bcff0114842c6b1cc287de82655e79
MalaikahKhalid <malaikahkhalid9@gmail.com>
2026-03-13 18:03:01 +0500

    first portfolio commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("My Portfolio")
+
+st.header("About Me")
+st.write("Hy my name is Ali Khalid.")
+st.write("I am a student interested in AI and technology.")
+
+st.header("Skills")
+st.write("- Python")
+st.write("- Machine Learning")
+st.write("- Data Analysis")
+
+st.header("Projects")
+st.write("1. AI Chatbot")
+st.write("2. Smart parking system")
+st.write("3. Portfolio Website")
+
+st.header("Contact")
+st.write("Email: itxali596@gmail.com")
+st.write("GitHub: github.com/Ali Khalid")
